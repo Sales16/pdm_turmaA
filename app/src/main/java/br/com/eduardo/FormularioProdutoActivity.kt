7e30fd83a8c2114c7e43a13882b0729978baa195
Eduardo <eduardo.sales.silva1603@gmail.com>
2024-09-23 10:55:51 -0300

    Descrição das alterações

---
+++ app/src/main/java/br/com/eduardo/FormularioProdutoActivity.kt
@@ -0,0 +1,11 @@
+package br.com.eduardo
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FormularioProdutoActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView((R.layout.formulario_produto_activity))
+    }
+}
